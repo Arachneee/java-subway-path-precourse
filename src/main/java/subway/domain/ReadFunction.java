d1ee46a1960f7f690cc8315ce18a83bb40c3edd5
Arachneee <jhg2819@naver.com>
2023-12-13 13:28:04 +0900

    feat: 경로 조회 기준 입 예외

---
+++ src/main/java/subway/domain/ReadFunction.java
@@ -0,0 +1,33 @@
+package subway.domain;
+
+import java.util.Arrays;
+import subway.exception.ErrorMessage;
+import subway.exception.SubwayException;
+
+public enum ReadFunction {
+
+    DISTANCE("1"),
+    TIME("2"),
+    BACK("B");
+
+    private final String value;
+
+    ReadFunction(String value) {
+        this.value = value;
+    }
+
+    public static ReadFunction from(final String value) {
+        return Arrays.stream(values())
+                .filter(mainFunction -> mainFunction.value.equals(value))
+                .findFirst()
+                .orElseThrow(() -> new SubwayException(ErrorMessage.INVALID_FUNCTION));
+    }
+
+    public boolean isDistance() {
+        return this.equals(DISTANCE);
+    }
+
+    public boolean isTime() {
+        return this.equals(TIME);
+    }
+}
