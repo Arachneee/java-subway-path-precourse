1e78381f7ca1fe098a6b0d7cd7ecba6256b1d497
Arachneee <jhg2819@naver.com>
2023-12-13 13:20:02 +0900

    feat: 예외 출력 후 재입력

---
+++ src/main/java/subway/util/ExceptionRoofer.java
@@ -0,0 +1,34 @@
+package subway.util;
+
+import java.util.function.Supplier;
+import subway.view.OutputView;
+
+
+public final class ExceptionRoofer {
+
+    private static final OutputView outputView = new OutputView();
+
+    private ExceptionRoofer() {
+    }
+
+    public static <T> T supply(final Supplier<T> method) {
+        while (true) {
+            try {
+                return method.get();
+            } catch (IllegalArgumentException illegalArgumentException) {
+                outputView.printError(illegalArgumentException.getMessage());
+            }
+        }
+    }
+
+    public static void run(final Runnable runnable) {
+        while (true) {
+            try {
+                runnable.run();
+                return;
+            } catch (IllegalArgumentException illegalArgumentException) {
+                outputView.printError(illegalArgumentException.getMessage());
+            }
+        }
+    }
+}
